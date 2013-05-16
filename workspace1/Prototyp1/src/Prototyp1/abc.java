4027843b40a80e4e6092045abafc62f31458225c
Victor Lite <lite.victor@yahoo.com>
2013-05-16 18:17:24 +0200

    test

---
+++ workspace1/Prototyp1/src/Prototyp1/abc.java
@@ -0,0 +1,24 @@
+package Prototyp1;
+import org.jgraph.*;
+import org.jgraph.graph.*;
+
+
+@SuppressWarnings("unused")
+public class abc {
+	public int node;
+	public int edge;
+	
+	public abc(int node, int edge){
+		this.edge=edge;
+		this.node=node;
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+ 
+	
+}
